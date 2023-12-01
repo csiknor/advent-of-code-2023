0aa11238d05cf3d5c642a7a590e2d739cba83441
csiknor <norbert.csik@icloud.com>
2023-12-01 22:45:40 +0100

    Day 1 / Task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,5 @@ import day1.Task1
 
 fun main() {
     Task1.solve1()
+    Task1.solve2()
 }
--- src/main/kotlin/day1/Task1.kt
+++ src/main/kotlin/day1/Task1.kt
@@ -12,4 +12,25 @@ object Task1 {
             .sumOf { "${it.first()}${it.last()}".toInt() }
             .let { println(it) }
     }
+
+    private val digits = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+    fun solve2() {
+        File(Task1.javaClass.getResource("sample2.txt")!!.toURI()).readLines()
+            .asSequence()
+            .onEach { print("$it ") }
+            .map { replaceInOrder(it) }
+            .onEach { print("$it ") }
+            .map { "${it.first()}${it.last()}".toInt() }
+            .onEach { println("$it ") }
+            .sum()
+            .let { println(it) }
+    }
+
+    private fun replaceInOrder(s: String) =
+        digits.keys.plus((1..9).map { it.toString() }).asSequence()
+            .flatMap { it.toRegex().findAll(s) }
+            .sortedBy { it.range.first }
+            .map { it.value }
+            .map { digits[it]?.toString() ?: it }
+            .joinToString("")
 }
