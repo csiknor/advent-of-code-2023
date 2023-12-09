a324146313527bb3b26de68f43197ae9324feb54
csiknor <norbert.csik@icloud.com>
2023-12-09 09:48:46 +0100

    feat: Day9 / Part 2

--- src/main/kotlin/day9/Task.kt
+++ src/main/kotlin/day9/Task.kt
@@ -4,20 +4,21 @@ import java.io.File
 
 object Task {
     fun solvePart1(filename: String) =
-        File(javaClass.getResource(filename)!!.toURI()).useLines { line -> process(line) }
+        File(javaClass.getResource(filename)!!.toURI()).useLines { line -> process(line) { predictLast(it) } }
 
     fun solvePart2(filename: String) =
-        File(javaClass.getResource(filename)!!.toURI()).useLines { line -> process(line) }
+        File(javaClass.getResource(filename)!!.toURI()).useLines { line -> process(line) { predictFirst(it) } }
 
-    fun process(lines: Sequence<String>) = parse(lines)
+    fun process(lines: Sequence<String>, predictor: (List<List<Long>>) -> Long) = parse(lines)
         .map { generateDifferences(listOf(it)) }
-        .map { predictLast(it) }
+        .map { predictor(it) }
         .sum()
 
-    private fun parse(lines: Sequence<String>) = lines.map { it.split(' ').map { it.toLong() } }
+    private fun parse(lines: Sequence<String>) = lines.map { line -> line.split(' ').map { it.toLong() } }
 }
 
-fun predictLast(series: List<List<Long>>) = series.reversed().sumOf { it.last() }
+fun predictFirst(series: List<List<Long>>) = series.map { it.first() }.reduceRight { a, b -> a - b }
+fun predictLast(series: List<List<Long>>) = series.map { it.last() }.reduce { a, b -> a + b }
 
-fun generateDifferences(curr: List<List<Long>>): List<List<Long>> = if (curr.last().sum() == 0L) curr
+fun generateDifferences(curr: List<List<Long>>): List<List<Long>> = if (curr.last().all { it == 0L }) curr
 else generateDifferences(curr.plusElement(curr.last().windowed(2) { (a, b) -> b - a }))
--- src/test/kotlin/day9/TaskSpec.kt
+++ src/test/kotlin/day9/TaskSpec.kt
@@ -16,15 +16,44 @@ class TaskSpec: StringSpec({
         )
     }
 
+    "generates differences correctly 2" {
+        generateDifferences(listOf(listOf(6L, 2L, -1L, -3L, -4L))) shouldBe listOf(
+            listOf(6L, 2L, -1L, -3L, -4L),
+            listOf(-4L, -3L, -2L, -1L),
+            listOf(1L, 1L, 1L),
+            listOf(0L, 0L),
+        )
+    }
+
     "predicts last" {
-        predictLast(listOf(
-            listOf(9L, 12L, 15L),
+        predictLast(
+            listOf(
+                listOf(9L, 12L, 15L),
+                listOf(3L, 3L),
+                listOf(0L),
+            )
+        ) shouldBe 18L
+    }
+
+    listOf(
+        listOf(
+            listOf(0L, 3L, 6L),
             listOf(3L, 3L),
             listOf(0L),
-        )) shouldBe 18L
+        ) to -3L,
+        listOf(
+            listOf(1L, 3L, 6L, 10L, 15L),
+            listOf(2L, 3L, 4L),
+            listOf(1L, 1L),
+            listOf(0L),
+        ) to 0L,
+    ).forEach { (series, expected) ->
+        "predicts first $expected" {
+            predictFirst(series) shouldBe expected
+        }
     }
 
     "solves sample 2" {
-        Task.solvePart2("sample.txt") shouldBe TODO()
+        Task.solvePart2("sample.txt") shouldBe 2
     }
 })
