ea6f7a2886751ab9a2fc12e2481585b928bb74ec
csiknor <norbert.csik@icloud.com>
2023-12-16 20:23:53 +0100

    feat: Day 14 / Task 2

--- src/main/kotlin/day14/Task.kt
+++ src/main/kotlin/day14/Task.kt
@@ -6,19 +6,52 @@ object Task {
     fun solvePart1(filename: String) =
         process(File(javaClass.getResource(filename)!!.toURI()).readLines())
 
-    fun solvePart2(filename: String) =
-        process(File(javaClass.getResource(filename)!!.toURI()).readLines())
+    fun solvePart2(filename: String, cycles: Int = 1_000_000_000) =
+        process2(File(javaClass.getResource(filename)!!.toURI()).readLines(), cycles)
 
-    fun process(lines: List<String>) = transpose(lines).map { line ->
+    private fun process(lines: List<String>) = transpose(lines).sumOf { line ->
         weighRocks(rollLeft(line))
-    }.sum()
+    }
+
+    private fun process2(lines: List<String>, cycles: Int) =
+        generateSequence(lines) { rollCycle(it) }
+            .map { transpose(it).sumOf { line -> weighRocks(line) } }
+            .take(1_000)
+            .toList()
+            .let { weights -> detectCycle(weights)!!
+                .let { (start, length) -> weights[start + (cycles - start) % length] }
+            }
+
+    fun rollCycle(lines: List<String>): List<String> {
+        val north = transpose(transpose(lines).map { rollLeft(it) })
+        val west = north.map { rollLeft(it) }
+        val south = transpose(transpose(west).map { rollRight(it) })
+        val east = south.map { rollRight(it) }
+        return east
+    }
 
     fun weighRocks(line: String) = line.mapIndexed { index, c -> when(c) { 'O' -> line.length - index else -> 0 } }.sum()
 
-    fun rollLeft(s: String) = s.split("#").map { part ->
-        part.count { it == 'O' }.let { c -> "O".repeat(c) + ".".repeat(part.length-c) }
+    fun rollLeft(s: String) = roll(s, 'O', '.')
+    fun rollRight(s: String) = roll(s, '.', 'O')
+
+    private fun roll(s: String, first: Char, last: Char) = s.split("#").map { part ->
+        part.count { it == first }.let { c -> first.toString().repeat(c) + last.toString().repeat(part.length-c) }
     }.joinToString("#")
 
     fun transpose(patterns: List<String>) = patterns
         .fold(List(patterns.first().length) { "" }) { acc, s -> acc.zip(s.toList()).map { (a, c) -> a + c } }
+
+    fun detectCycle(numbers: List<Int>): Pair<Int, Int>? {
+        for (len in 1..<(numbers.size/2)) {
+            for (i in 0..<(numbers.size/2)) {
+                val cycleCount = (numbers.size - i) / len - 1
+                val remainingCount = (numbers.size - i) % len
+                if ((1..cycleCount).all { numbers.subList(i, i+len) == numbers.subList(i+it*len, i+it*len+len) }
+                    && numbers.subList(i, i+remainingCount) == numbers.subList(numbers.size-remainingCount, numbers.size)
+                ) return i to len
+            }
+        }
+        return null
+    }
 }
--- src/test/kotlin/day14/TaskSpec.kt
+++ src/test/kotlin/day14/TaskSpec.kt
@@ -9,7 +9,60 @@ class TaskSpec: StringSpec({
     }
 
     "solves sample 2" {
-        Task.solvePart2("sample.txt") shouldBe TODO()
+        Task.solvePart2("sample.txt") shouldBe 64
+    }
+
+    "roll cycle" {
+        val input = """
+            O....#....
+            O.OO#....#
+            .....##...
+            OO.#O....O
+            .O.....O#.
+            O.#..O.#.#
+            ..O..#O..O
+            .......O..
+            #....###..
+            #OO..#....
+        """.trimIndent().split("\n")
+        Task.rollCycle(input) shouldBe """
+            .....#....
+            ....#...O#
+            ...OO##...
+            .OO#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#....
+            ......OOOO
+            #...O###..
+            #..OO#....
+        """.trimIndent().split("\n")
+
+        Task.rollCycle(Task.rollCycle(input)) shouldBe """
+            .....#....
+            ....#...O#
+            .....##...
+            ..O#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#...O
+            .......OOO
+            #..OO###..
+            #.OOO#...O
+        """.trimIndent().split("\n")
+
+        Task.rollCycle(Task.rollCycle(Task.rollCycle(input))) shouldBe """
+            .....#....
+            ....#...O#
+            .....##...
+            ..O#......
+            .....OOO#.
+            .O#...O#.#
+            ....O#...O
+            .......OOO
+            #...O###.O
+            #.OOO#...O
+        """.trimIndent().split("\n")
     }
 
     listOf(
@@ -26,11 +79,65 @@ class TaskSpec: StringSpec({
     }
 
     listOf(
+        "O....#...." to "....O#....",
+        "..O..#O..O" to "....O#..OO",
+        "..O#.#O..O" to "..O#.#..OO",
+        "..O#.##..O" to "..O#.##..O",
+        "#.O#.##..O" to "#.O#.##..O",
+        "#.O#.##.O#" to "#.O#.##.O#",
+    ).forEach { (input, expected) ->
+        "rolls $input" {
+            Task.rollRight(input) shouldBe expected
+        }
+    }
+
+    listOf(
+        "O....#...." to 10,
+        "O....#OO.." to 17,
+        "O..#.#OO.." to 17,
+        "O..#.##O.." to 13,
+    ).forEach { (input, weight) ->
+        "weight rocks $input" {
+            Task.weighRocks(input) shouldBe weight
+        }
+    }
+
+    listOf(
         listOf("ab", "cd") to listOf("ac", "bd"),
         listOf("123", "abc") to listOf("1a", "2b", "3c"),
     ).forEach { (input, expected) ->
         "transposes $input" {
             Task.transpose(input) shouldBe expected
+        }
+    }
+
+    listOf(
+        listOf("ab", "cd") to listOf("ca", "db"),
+        listOf("123", "abc") to listOf("a1", "b2", "c3"),
+    ).forEach { (input, expected) ->
+        "4 transpose $input" {
+            Task.transpose(Task.transpose(Task.transpose(Task.transpose(input)))) shouldBe input
+        }
+    }
+
+    listOf(
+        listOf(
+            "..#",
+            "#.#",
+            "...",
+        ),
+    ).forEach { input ->
+        "cycle $input" {
+            Task.rollCycle(input) shouldBe input
+        }
+    }
+
+    listOf(
+        listOf(2, 0, 6, 3, 1, 6, 3, 1, 6, 3, 1, 6, 3, 1, 6, 3, 1) to listOf(6, 3, 1),
+        listOf(2, 0, 6, 3, 6, 6, 5, 3, 1, 6, 5, 3, 1, 6, 5, 3, 1, 6, 5, 3, 1, 6, 5, 3, 1, 6, 5, 3) to listOf(6, 5, 3, 1),
+    ).forEach { (numbers, cycle) ->
+        "detect cycle $cycle" {
+            Task.detectCycle(numbers)!!.let { (start, length) -> numbers.subList(start, start + length).also { println("Cycle: $start [$length]") } } shouldBe cycle
         }
     }
 })
