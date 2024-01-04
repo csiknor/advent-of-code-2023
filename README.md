025f35a4a538a257785cf7e2c30fa75d7e6c1c06
csiknor <norbert.csik@icloud.com>
2024-01-04 14:13:06 +0100

    refactor+docs: Day 18

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ For the detailed solutions, visit the individual days and tasks:
 * Day 15: [Task](src/main/kotlin/day15/Task.kt)
 * Day 16: [Task](src/main/kotlin/day16/Task.kt)
 * Day 17: [Task](src/main/kotlin/day17/Task.kt)
+* Day 18: [Task](src/main/kotlin/day18/Task.kt)
--- src/main/kotlin/day18/Task.kt
+++ src/main/kotlin/day18/Task.kt
@@ -4,9 +4,20 @@ import java.io.File
 import kotlin.math.abs
 
 object Task {
+    /*
+    Initially I tried to use day 10's solution to calculate the surface, which worked well for part 1, but failed
+    miserably for part 2. Then I went on and learned about Pick's theorem and how one can calculate the surface of
+    arbitrary polygons easily, which I implemented here.
+    After parsing the input into a sequence of digs, we identify all the vertices of the polygon created by all the
+    digs. We use the shoelace formula to calculate the area of such polygon along with the manhattan distance to find
+    its circumference which are then fed into Pick's theorem to get the final answer.
+     */
     fun solvePart1(filename: String) =
         File(javaClass.getResource(filename)!!.toURI()).useLines { line -> process(parse(line)) }
 
+    /*
+    In part 2 we parse the input using the alternative representation of the digs and follow suit.
+     */
     fun solvePart2(filename: String) =
         File(javaClass.getResource(filename)!!.toURI()).useLines { line ->
             process(parse(line).map { it.alternative() })
